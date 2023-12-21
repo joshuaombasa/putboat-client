09b872f7c9fe9351d70ffc85bd52c8524580b77e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-21 10:21:51 +0300

    add build page

---
+++ src/pages/merchant/Income.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Income() {
+    return (
+        <div className="income-page-container">
+            <h1>This is merchant income page</h1>
+        </div>
+    )
+}
