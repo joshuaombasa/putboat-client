6cf3e5aabb2768ca6806983a2bd7aef435cfe7dd
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-21 10:20:37 +0300

    add build page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default function NotFound() {
+    return (
+        <div className="not-found-page-container">
+            <div className="not-found-text">
+                <h1>Sorry, the page you were looking for was not found.</h1>
+                <Link to='/'>Return to home</Link>
+            </div>
+        </div>
+    )
+}
