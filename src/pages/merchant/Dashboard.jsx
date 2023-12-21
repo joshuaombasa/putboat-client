c019c37d8d27929caecd0f29706499f11304c2ac
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-21 10:22:04 +0300

    add build page

---
+++ src/pages/merchant/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dashboard() {
+    return (
+        <div className="dashboard-page-container">
+            <h1>This is merchant Dashboard page</h1>
+        </div>
+    )
+}
