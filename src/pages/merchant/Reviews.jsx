d7d8d26c44234964a2da0b085b3fcd18d0e9297e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-21 10:21:17 +0300

    add build page

---
+++ src/pages/merchant/Reviews.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Reviews() {
+    return (
+        <div className="reviews-page-container">
+            <h1>This is reviews page</h1>
+        </div>
+    )
+}
