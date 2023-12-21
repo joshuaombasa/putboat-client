f864a6f6c30be01c36c8d9eb5a8a406c3cf0b67b
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-21 10:23:29 +0300

    add build component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+export default function Layout() {
+    return (
+        <div className="layout--container">
+            <Header />
+            <div className="outlet-container">
+                <Outlet />
+            </div>
+            <Footer/>
+        </div>
+    )
+}
