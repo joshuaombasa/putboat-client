7c2bbfe644cc0b3898535ea9cf18bea2725c2712
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-22 11:38:08 +0300

    configure loader to route and component

--- src/pages/merchant/MerchantBoatDetails.jsx
+++ src/pages/merchant/MerchantBoatDetails.jsx
@@ -1,56 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { NavLink, Outlet, useParams, Link } from "react-router-dom";
+import React, { Suspense, useEffect, useState } from "react";
+import { NavLink, Outlet, useParams, Link, defer, useLoaderData, Await } from "react-router-dom";
+import { getBoat } from "../../utils/api";
+
+export async function loader({ request, params }) {
+   return defer({ merchantBoat: getBoat(params.id) })
+}
 
 export default function MerchantBoatDetails() {
 
-    const [selectedMerchantBoat, setSelectedMerchantBoat] = useState(null)
-    const [error, setError] = useState(false)
+    const dataPromise = useLoaderData()
 
-    const params = useParams()
-
-    useEffect(() => {
-        async function getSelectedMerchantBoat() {
-            const res = await fetch(`http://localhost:3000/api/boats/${params.id}`)
-            const data = await res.json()
-            if (!res.ok) {
-                setError(true)
-            }
-            setSelectedMerchantBoat(data[0])
-        }
-
-        getSelectedMerchantBoat()
-    }, [])
-
-    
-
-    if (!selectedMerchantBoat) {
+    function renderMerchantBoat(selectedMerchantBoat) {
         return (
-            <h1>Loading...</h1>
+            <>
+                <Link
+                    to={`..`}
+                    relative='path'
+                >&larr; Back to all boats.</Link>
+                <div className="merchant-boat-details-top">
+                    <img src={selectedMerchantBoat.imageUrl} alt="" />
+                    <div className="top-info">
+                        <span>{selectedMerchantBoat.type}</span>
+                        <h3>{selectedMerchantBoat.name}</h3>
+                        <h3>$ {selectedMerchantBoat.price}/day</h3>
+                    </div>
+                </div>
+                <div className="merchant-boat-details-bottom">
+                    <nav className="merchant-boat-details-nav">
+                        <NavLink to='.'>Details</NavLink>
+                        <NavLink to='pricing'>Pricing</NavLink>
+                        <NavLink to='photos'>Photos</NavLink>
+                    </nav>
+                    <Outlet context={{ selectedMerchantBoat }} />
+                </div>
+            </>
         )
     }
 
     return (
         <div className="merchant-boat-details-page-container">
-            <Link
-               to={`..`}
-               relative='path'
-            >&larr; Back to all boats.</Link>
-            <div className="merchant-boat-details-top">
-                <img src={selectedMerchantBoat.imageUrl} alt="" />
-                <div className="top-info">
-                    <span>{selectedMerchantBoat.type}</span>
-                    <h3>{selectedMerchantBoat.name}</h3>
-                    <h3>$ {selectedMerchantBoat.price}/day</h3>
-                </div>
-            </div>
-            <div className="merchant-boat-details-bottom">
-                <nav className="merchant-boat-details-nav">
-                    <NavLink to='.'>Details</NavLink>
-                    <NavLink to='pricing'>Pricing</NavLink>
-                    <NavLink to='photos'>Photos</NavLink>
-                </nav>
-                <Outlet context={{selectedMerchantBoat}}/>
-            </div>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.merchantBoat}>
+                    { renderMerchantBoat }
+                </Await>
+            </Suspense>
         </div>
     )
 }
