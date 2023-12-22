fc0f8f4fd8a3c87b22c06575fe64dd14ba193767
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-22 11:36:17 +0300

    update page logic to route and component

--- src/pages/boats/Boats.jsx
+++ src/pages/boats/Boats.jsx
@@ -1,48 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { Suspense, useEffect, useState } from "react";
+import { Await, defer, Link, useLoaderData } from "react-router-dom";
+import { getBoats } from "../../utils/api";
+
+export async function loader() {
+
+    return defer({ boats: getBoats() })
+}
 
 export default function Boats() {
 
-    const [error, setError] = useState(false)
-    const [boats, setBoats] = useState([])
+    const boatsPromise = useLoaderData()
 
-    useEffect(() => {
-        async function getBoats() {
-            const res = await fetch('http://localhost:3000/api/boats')
-            
-            const data = await res.json()
-            if(!res.ok) {
-                setError(true)
-            }
-            setBoats(data)
-        }
 
-        getBoats()
-    },[])
 
-    if (boats.length === 0) {
+    function renderBoats(boats) {
+
+        const boatNames = boats.map(boat => (
+            <Link to={boat.id} key={boat.id} className="boatItem">
+                <img src={boat.imageUrl} alt="" />
+                <div className="boat-item-details">
+                    <h3>{boat.name}</h3>
+                    <h3>$ {boat.price}/day</h3>
+                </div>
+                <span>{boat.type}</span>
+            </Link>
+        ))
+
         return (
-            <h1>Loading...</h1>
+            <>
+                <h1>Explore our boat options.</h1>
+                <div className="boatItems">
+                    {boatNames}
+                </div>
+            </>
         )
     }
 
-    const boatNames = boats.map(boat => (
-        <Link to={boat.id} key={boat.id} className="boatItem">
-            <img src={boat.imageUrl} alt="" />
-            <div className="boat-item-details">
-                <h3>{boat.name}</h3>
-                <h3>$ {boat.price}/day</h3>
-            </div>
-            <span>{boat.type}</span>
-        </Link>
-    ))
-
     return (
         <div className="boats-page-container">
-            <h1>Explore our boat options.</h1>
-            <div className="boatItems">
-                {boatNames}
-            </div>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={boatsPromise.boats}>
+                    { renderBoats }
+                </Await>
+            </Suspense>
         </div>
     )
 }
