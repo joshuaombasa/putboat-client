449a80a9146f96665d5a1b32fd9369a999c2450d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-22 11:38:35 +0300

    configure loader to route and component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import './App.css'
 import {createBrowserRouter, Route, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Boats from './pages/boats/Boats'
-import SelectedBoat from './pages/boats/SelectedBoat'
+import Boats, {loader as boatsLoader} from './pages/boats/Boats'
+import SelectedBoat, {loader as selectedBoatLoader} from './pages/boats/SelectedBoat'
 import Dashboard from './pages/merchant/Dashboard'
 import Income from './pages/merchant/Income'
-import MerchantBoats from './pages/merchant/MerchantBoats'
+import MerchantBoats, {loader as merchantBoatsLoader} from './pages/merchant/MerchantBoats'
 import Reviews from './pages/merchant/Reviews'
 import Layout from './components/Layout'
 import Login from './pages/Login'
 import NotFound from './pages/NotFound'
 import MerchantLayout from './components/MerchantLayout'
-import MerchantBoatDetails from './pages/merchant/MerchantBoatDetails'
+import MerchantBoatDetails, {loader as merchantBoatDetailsLoader} from './pages/merchant/MerchantBoatDetails'
 import SelectedMerchantBoatDetails from './components/SelectedMerchantBoatDetails'
 import SelectedMerchantBoatPricing from './components/SelectedMerchantBoatPricing'
 import SelectedMerchantBoatPhotos from './components/SelectedMerchantBoatPhotos'
@@ -25,13 +25,13 @@ function App() {
      <Route path='/' element={<Layout/>}>
         <Route index  element={<Home/>}/>
         <Route path='about' element={<About/>}/>
-        <Route path='boats' element={<Boats/>}/>
-        <Route path='boats/:id' element={<SelectedBoat/>}/>
+        <Route path='boats' loader={boatsLoader} element={<Boats/>}/>
+        <Route path='boats/:id' loader={selectedBoatLoader} element={<SelectedBoat/>}/>
         <Route path='merchant' element={<MerchantLayout/>}>
           <Route index  element={<Dashboard/>}/>
           <Route path='income'  element={<Income/>}/>
-          <Route path='boats'  element={<MerchantBoats/>}/>
-          <Route path='boats/:id' element={<MerchantBoatDetails/>}>
+          <Route path='boats' loader={merchantBoatsLoader} element={<MerchantBoats/>}/>
+          <Route path='boats/:id' loader={merchantBoatDetailsLoader} element={<MerchantBoatDetails/>}>
             <Route index element={<SelectedMerchantBoatDetails/>}/>
             <Route path='pricing' element={<SelectedMerchantBoatPricing/>}/>
             <Route path='photos'  element={<SelectedMerchantBoatPhotos/>}/>
