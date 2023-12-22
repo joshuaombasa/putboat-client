fa9bc88cee4697a99f14838a92b3842ba786bbea
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-22 11:37:02 +0300

    configure loader to route and component

--- src/pages/boats/SelectedBoat.jsx
+++ src/pages/boats/SelectedBoat.jsx
@@ -1,43 +1,42 @@
-import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import React, { Suspense, useEffect, useState } from "react";
+import { useParams, Link, defer, useLoaderData, Await } from "react-router-dom";
+import { getBoat } from "../../utils/api";
+
+export async function loader({ request, params }) {
+
+    return defer({ boat: getBoat(params.id) })
+}
 
 export default function SelectedBoat() {
 
-    const [boat, setBoat] = useState(null)
-    const [error, setError] = useState(false)
+    const dataPromise = useLoaderData()
 
-    const params = useParams()
-
-    useEffect(()  => {
-        async function getBoat() {
-            const res = await fetch(`http://localhost:3000/api/boats/${params.id}`)
-            const data  = await res.json()
-            if (!res.ok) {
-                setError(error)
-            }
-            setBoat(data[0])
-        }
-        getBoat()
-    },[])
-
-
-    if (!boat) {
+    function renderBoat(boat) {
+        
         return (
-            <h1>Loading...</h1>
+            <>
+                <Link
+                    to={`..`}
+                    relative='path'
+                >&larr; Back to all boats.</Link>
+                <img src={boat.imageUrl} alt="" />
+                <span>{boat.type}</span>
+                <h3>{boat.name}</h3>
+                <h3>$ {boat.price}/day</h3>
+                <p>{boat.description}</p>
+                <Link className="rent-van-link">Rent this van</Link>
+            </>
         )
     }
+
+
     return (
         <div className="selected-boat-page-container">
-            <Link
-               to={`..`}
-               relative='path'
-            >&larr; Back to all boats.</Link>
-            <img src={boat.imageUrl} alt="" />
-            <span>{boat.type}</span>
-            <h3>{boat.name}</h3>
-            <h3>$ {boat.price}/day</h3>
-            <p>{boat.description}</p>
-            <Link className="rent-van-link">Rent this van</Link>
+            <Suspense  fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.boat}>
+                    {renderBoat}
+                </Await>
+            </Suspense>
         </div>
     )
 }
