79299f04f7f189f2dcf2809f0a17165619c4b7c8
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-22 11:38:16 +0300

    configure loader to route and component

--- src/pages/merchant/MerchantBoats.jsx
+++ src/pages/merchant/MerchantBoats.jsx
@@ -1,48 +1,47 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { Suspense, useEffect, useState } from "react";
+import { Await, defer, Link, useLoaderData } from "react-router-dom";
+import { getMerchantBoats } from "../../utils/api";
+
+export async function loader() {
+    return defer({ merchantBoats: getMerchantBoats() })
+}
 
 export default function MerchantBoats() {
 
-    const [merchantBoats, setMerchantBoats] = useState([])
-    const [error, setError] = useState(false)
+    const dataPromise = useLoaderData()
 
-    useEffect(() => {
-        async function getMerchantBoats() {
-            const res = await fetch('http://localhost:3000/api/merchantBoats')
-            const data = await res.json()
-            if (!res.ok) {
-                setError(true)
-            }
-            setMerchantBoats(data)
-        }
 
-        getMerchantBoats()
-    },[])
+    function renderMerchantBoats(merchantBoats) {
 
-    if (merchantBoats.length === 0) {
+        const merchantBoatsElements = merchantBoats.map(boat => (
+            <Link key={boat.id} to={boat.id} className="merchant-boat-item">
+                <img src={boat.imageUrl} alt="" />
+                <div className="more-info">
+                    <h3>{boat.name}</h3>
+                    <span>$ {boat.price}/day</span>
+                </div>
+            </Link>
+        ))
+
         return (
-            <h1>Loading...</h1>
+            <>
+                <h1>Your listed boats</h1>
+                <div className="merchant-boats">
+                    {
+                        merchantBoatsElements
+                    }
+                </div>
+            </>
         )
     }
 
-    const merchantBoatsElements = merchantBoats.map(boat => (
-        <Link key={boat.id} to={boat.id} className="merchant-boat-item">
-            <img src={boat.imageUrl} alt="" />
-            <div className="more-info">
-                <h3>{boat.name}</h3>
-                <span>$ {boat.price}/day</span>
-            </div>
-        </Link>
-    ))
-
     return (
         <div className="merchat-boats-page-container">
-            <h1>Your listed boats</h1>
-            <div className="merchant-boats">
-                {
-                    merchantBoatsElements
-                }
-            </div>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.merchantBoats}>
+                    {renderMerchantBoats}
+                </Await>
+            </Suspense>
         </div>
     )
 }
