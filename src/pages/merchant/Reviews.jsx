c8c13e8c0affc37e0ec0e836e83828e11e955f66
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-23 12:41:30 +0300

    feat: Add authentication protection to React routes
    This commit implements authentication checks to secure specific routes in the React application. Protected routes now require authentication, ensuring that only authorized users can access them. The changes include updates to route configuration and the integration of authentication guards to enhance application security.

--- src/pages/merchant/Reviews.jsx
+++ src/pages/merchant/Reviews.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { authenticateUser } from "../../utils/auth";
+
+
+export async function loader({request}) {
+    const url = new URL(request.url)
+    const pathname = url.pathname
+    
+    authenticateUser(pathname)
+    return  null
+}
 
 export default function Reviews() {
     return (
