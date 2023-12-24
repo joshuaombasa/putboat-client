54135a49c342cfaefa4dbf800487ce97e2066d9c
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-24 11:51:07 +0300

    updated component logic to handle login errors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,16 +5,18 @@ import { handleLogin } from "../utils/api";
 export default function Login() {
 
     const [formData, setFormData] = useState({
-        email:'',
+        email: '',
         password: ''
     })
+
+    const [error, setError] = useState('')
 
     const [searchParams, setSearchParams] = useSearchParams()
 
     const navigate = useNavigate()
 
     function handleChange(event) {
-        const { name, value} = event.target
+        const { name, value } = event.target
         setFormData(prevState => {
             return {
                 ...prevState,
@@ -25,41 +27,45 @@ export default function Login() {
 
     const sp = new URLSearchParams(searchParams)
 
-    const redirectToPath = sp.get('redirectTo') ? sp.get('redirectTo'): '/merchant'
+    const redirectToPath = sp.get('redirectTo') ? sp.get('redirectTo') : '/merchant'
 
-    console.log(redirectToPath)
 
     async function handleSubmit(event) {
         event.preventDefault()
-        const data = await handleLogin(formData)
-        localStorage.setItem('isLoggedIn', true)
-        navigate(`${redirectToPath}`)
+        try {
+            const data = await handleLogin(formData)
+            localStorage.setItem('isLoggedIn', true)
+            navigate(`${redirectToPath}`)
+        } catch (error) {
+            setError(error)
+        }
     }
 
     return (
         <div className="login-page-container">
             <div className="login-page-text">
                 <h1>Sign in to your account</h1>
+                {error && <h3 className="error-style">{error.message}</h3>}
                 <form onSubmit={handleSubmit}>
-                    <input 
-                         type="text" 
-                         className="login-email"
-                         placeholder="Email address"
-                         name="email"
-                         onChange={handleChange}
-                         value={formData.email}
+                    <input
+                        type="text"
+                        className="login-email"
+                        placeholder="Email address"
+                        name="email"
+                        onChange={handleChange}
+                        value={formData.email}
                     />
-                    <input 
-                         type="text" 
-                         className="login-password"
-                         placeholder="Password"
-                         name="password"
-                         onChange={handleChange}
-                         value={formData.password}
+                    <input
+                        type="text"
+                        className="login-password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleChange}
+                        value={formData.password}
                     />
 
                     <button>Sign in</button>
-                    
+
                 </form>
             </div>
         </div>
