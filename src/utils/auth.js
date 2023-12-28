145b29a19c2f10b8e43fbfac9b45962f0f72ecef
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-28 23:55:27 +0300

    updated auth logic

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom"
 
 export function authenticateUser(pathname) {
     const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'
-
+    
     if (!isLoggedIn) {
         // throw redirect('/login')
         throw redirect(`/login?redirectTo=${pathname}`)
