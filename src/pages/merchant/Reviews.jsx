9f306f8236ccbffeb05e0a27f3490c0ef145deef
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-12-29 22:48:44 +0300

    updated page logic

--- src/pages/merchant/Reviews.jsx
+++ src/pages/merchant/Reviews.jsx
@@ -5,7 +5,6 @@ import { authenticateUser } from "../../utils/auth";
 export async function loader({request}) {
     const url = new URL(request.url)
     const pathname = url.pathname
-    
     authenticateUser(pathname)
     return  null
 }
