3ce0e4c14efc46177ed90391838ee570e989a6ec
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-14 19:39:55 +0300

    add data-testid attributes to help test the component

--- src/pages/boats/SelectedBoat.jsx
+++ src/pages/boats/SelectedBoat.jsx
@@ -2,41 +2,81 @@ import React, { Suspense, useEffect, useState } from "react";
 import { useParams, Link, defer, useLoaderData, Await } from "react-router-dom";
 import { getBoat } from "../../utils/api";
 
-export async function loader({ request, params }) {
+// export async function loader({ request, params }) {
 
-    return defer({ boat: getBoat(params.id) })
-}
+//     return defer({ boat: getBoat(params.id) })
+// }
 
 export default function SelectedBoat() {
 
-    const dataPromise = useLoaderData()
+    const {id} = useParams()
 
-    function renderBoat(boat) {
-        
-        return (
-            <>
-                <Link
-                    to={`..`}
-                    relative='path'
-                >&larr; Back to all boats.</Link>
-                <img src={boat.imageUrl} alt="" />
-                <span>{boat.type}</span>
-                <h3>{boat.name}</h3>
-                <h3>$ {boat.price}/day</h3>
-                <p>{boat.description}</p>
-                <Link className="rent-van-link">Rent this van</Link>
-            </>
-        )
+    const [boat, setBoat] = useState(null)
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch(`http://localhost:3000/api/boats/${id}`)
+                const data = await res.json()
+                setBoat(data)
+                
+            } catch (error) {
+
+            } finally {
+
+            }
+        }
+
+        fetchData()
+    },[])
+
+    if (!boat) {
+        return (<h1>Loading...</h1>)
     }
 
+    // const dataPromise = useLoaderData()
+
+    // function renderBoat(boat) {
+
+    //     return (
+    //         <>
+    //             <Link
+    //                 to={`..`}
+    //                 relative='path'
+    //             >&larr; Back to all boats.</Link>
+    //             <img src={boat.imageUrl} alt="" />
+    //             <span>{boat.type}</span>
+    //             <h3>{boat.name}</h3>
+    //             <h3>$ {boat.price}/day</h3>
+    //             <p>{boat.description}</p>
+    //             <Link className="rent-van-link">Rent this van</Link>
+    //         </>
+    //     )
+    // }
+
+
+    // return (
+    //     <div className="selected-boat-page-container">
+    //         <Suspense  fallback={<h1>Loading...</h1>}>
+    //             <Await resolve={dataPromise.boat}>
+    //                 {renderBoat}
+    //             </Await>
+    //         </Suspense>
+    //     </div>
+    // )
 
     return (
         <div className="selected-boat-page-container">
-            <Suspense  fallback={<h1>Loading...</h1>}>
-                <Await resolve={dataPromise.boat}>
-                    {renderBoat}
-                </Await>
-            </Suspense>
+            <Link
+                to={`..`}
+                relative='path'
+            >&larr; Back to all boats.</Link>
+            <img src={boat.imageUrl} alt="" />
+            <span>{boat.type}</span>
+            <h3>{boat.name}</h3>
+            <h3>$ {boat.price}/day</h3>
+            <p>{boat.description}</p>
+            <Link className="rent-van-link">Rent this van</Link>
         </div>
     )
 }
