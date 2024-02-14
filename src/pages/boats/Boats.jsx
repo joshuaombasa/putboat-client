3cdb203d087595594901f7bcd81520d45aeb82b3
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-14 19:39:22 +0300

    add data-testid attributes to help test the component

--- src/pages/boats/Boats.jsx
+++ src/pages/boats/Boats.jsx
@@ -2,47 +2,89 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Await, defer, Link, useLoaderData } from "react-router-dom";
 import { getBoats } from "../../utils/api";
 
-export async function loader() {
+// export async function loader() {
 
-    return defer({ boats: getBoats() })
-}
+//     return defer({ boats: getBoats() })
+// }
 
 export default function Boats() {
 
-    const boatsPromise = useLoaderData()
+    // const boatsPromise = useLoaderData()
+
+    const [boats, setBoats] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/api/boats');
+                const jsonData = await response.json();
+                setBoats(jsonData);
+
+            } catch (error) {
+                // setError(error);
+            } finally {
+                // setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [])
 
 
+    // function renderBoats(boats) {
 
-    function renderBoats(boats) {
+    //     const boatNames = boats.map(boat => (
+    //         <Link to={boat.id} key={boat.id} className="boatItem">
+    //             <img src={boat.imageUrl} alt="" />
+    //             <div className="boat-item-details">
+    //                 <h3>{boat.name}</h3>
+    //                 <h3>$ {boat.price}/day</h3>
+    //             </div>
+    //             <span>{boat.type}</span>
+    //         </Link>
+    //     ))
 
-        const boatNames = boats.map(boat => (
-            <Link to={boat.id} key={boat.id} className="boatItem">
-                <img src={boat.imageUrl} alt="" />
-                <div className="boat-item-details">
-                    <h3>{boat.name}</h3>
-                    <h3>$ {boat.price}/day</h3>
-                </div>
-                <span>{boat.type}</span>
-            </Link>
-        ))
+    //     return (
+    //         <>
+    //             <h1>Explore our boat options.</h1>
+    //             <div className="boatItems">
+    //                 {boatNames}
+    //             </div>
+    //         </>
+    //     )
+    // }
 
-        return (
-            <>
-                <h1>Explore our boat options.</h1>
-                <div className="boatItems">
-                    {boatNames}
-                </div>
-            </>
-        )
+    if (boats.length === 0) {
+        return (<h1>Loading...</h1>)
     }
+
+    const boatNames = boats.map(boat => (
+        <Link to={boat.id} key={boat.id} className="boatItem">
+            <img src={boat.imageUrl} alt="" />
+            <div className="boat-item-details">
+                <h3>{boat.name}</h3>
+                <h3>$ {boat.price}/day</h3>
+            </div>
+            <span>{boat.type}</span>
+        </Link>
+    ))
 
     return (
         <div className="boats-page-container">
-            <Suspense fallback={<h1>Loading...</h1>}>
-                <Await resolve={boatsPromise.boats}>
-                    { renderBoats }
-                </Await>
-            </Suspense>
+            <h1>Explore our boat options.</h1>
+            <div className="boatItems">
+                {boatNames}
+            </div>
         </div>
     )
+
+    // return (
+    //     <div className="boats-page-container">
+    //         <Suspense fallback={<h1>Loading...</h1>}>
+    //             <Await resolve={boatsPromise.boats}>
+    //                 { renderBoats }
+    //             </Await>
+    //         </Suspense>
+    //     </div>
+    // )
 }
